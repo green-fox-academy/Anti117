da33601afcf11a88b256ad0c0ab43e5637e4a44e
Anti <curiaflair@gmail.hu>
2018-10-26 13:43:50 +0200

    introduce

---
+++ weak-1/day-2/excercise4/introduce.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Antal Kovacs" << std::endl;
+    std::cout << "26" << std::endl;
+    std::cout << "1.70" << std::endl;
+    
+    return 0;
+}
