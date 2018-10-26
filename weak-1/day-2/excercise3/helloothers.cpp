9297de4e62b6fa9d31bb6cc599d0276d3c32d367
Anti <curiaflair@gmail.hu>
2018-10-26 13:35:35 +0200

    helloothers

---
+++ weak-1/day-2/excercise3/helloothers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello Zoli!" << std::endl;
+    std::cout << "Hello Judit!" << std::endl;
+    std::cout << "Hello Gergo!" << std::endl;
+
+    return 0;
+}
