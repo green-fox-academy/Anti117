9494e41909a3322b2419f1826f1551b108fcb826
Anti <curiaflair@gmail.hu>
2018-10-28 14:11:28 +0100

    hellouser

---
+++ week-01/day-2/exercise13/hellouser.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    int username;
+
+    std::cout << "Please enter a username";
+    std::cin >> username;
+
+    std::cout << "Hello"  << username << std::endl;
+
+    return 0;
+}
