2778107f101c24e66e3f8645e87bb8550f7be051
Anti <curiaflair@gmail.hu>
2018-10-28 14:02:09 +0100

    myfavorite

---
+++ week-01/day-2/exercise7/myfavorite.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+   int mynumber;
+   std::cout << " What is your favorite number?" << std::endl;
+   std::cin >> mynumber;
+   std::cout << "My favorite number is:" << mynumber << std::endl;
+    return 0;
+}
