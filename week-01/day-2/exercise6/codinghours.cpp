03a296747ee50b7b4962643d36a142f93f36cbcf
Anti <curiaflair@gmail.hu>
2018-10-28 14:01:02 +0100

    codinghours

---
+++ week-01/day-2/exercise6/codinghours.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    int hours = 5;
+    int semester = 17;
+    int dayly = 6;
+    std::cout << (hours*semester)*dayly << std::endl;
+
+    int number = 510;
+    std::cout << ((float)510/(52*17)) *100 << std::endl;
+    return 0;
+}
