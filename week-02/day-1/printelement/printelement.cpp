6d6b66403ba986e86ccb923d412aef9d318db384
Anti <curiaflair@gmail.hu>
2018-10-30 22:31:51 +0100

    PrintElement

---
+++ week-02/day-1/printelement/printelement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numbers [] = {4,5,6,7};
+    for(int a = 0; a < 4; a++)
+        cout << numbers[a] << endl;
+
+    return 0;
+}
