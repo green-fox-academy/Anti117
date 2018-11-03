f2de9f3f6d7953fef5b3a7e6ee41fad47ce1df2e
Anti <curiaflair@gmail.hu>
2018-11-03 10:11:12 +0100

    Colors

---
+++ week-02/day-1/colors/colors.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* args[]) {
+
+    string colors [5][3][4] = {{"lime","forest green","olive","pale green","spring green"},{"orangered","red","tomato"},{"orchid","violet","pink","hot pink"}};
+    cout << colors << endl;
+
+    return 0;
+}
