f231f0fc837d83b48a742fa9e2511b21e0a9379e
Anti <curiaflair@gmail.hu>
2018-11-13 17:21:07 +0100

    MainAnimal

---
+++ week-04/day-2/Animal/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "animal.h"
+
+int main() {
+    Animal animal;
+
+    std::cout << "Hunger: " << animal.getHunger() << " Thirst:" <<animal.getThirst() << std::endl;
+    animal.play();
+    std::cout << "Hunger: " << animal.getHunger() << " Thirst:" <<animal.getThirst() << std::endl;
+    animal.drink();
+    std::cout << "Thirst:" <<animal.getThirst() << std::endl;
+    animal.eat();
+    std::cout << "Hunger: " << animal.getHunger() << std::endl;
+
+    return 0;
+}
