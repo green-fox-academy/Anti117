fcaf2e2a404c0b15a535999c5b6480dffccad1e0
Anti <curiaflair@gmail.hu>
2018-11-15 13:23:39 +0100

    BunnyAgain

---
+++ week-04/day-4/Bunny2/BunnyAgain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int BunnyEars(int bunny);
+
+int main() {
+    int bunny;
+    std::cout << "Type in the number of Bunnies: " << std::endl;
+    std::cin >> bunny;
+    std::cout << "The bunnies have in total" << " " << BunnyEars(bunny)  << " ears!" << std::endl;
+    return 0;
+}
+
+int BunnyEars(int bunny){
+    if(bunny == 0)
+        return 0;
+    if(bunny % 2 == 1)
+        return 2 + BunnyEars(bunny-1);
+    return 3 + BunnyEars(bunny-1);
+}
